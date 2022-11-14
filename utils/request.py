a48ce1b5ed12bcf9e5ab6ba50b2799e378dafebb
farhansyedain <farhansyedain@gmail.com>
2022-11-14 16:30:06 +0530

    func to get updatible fields

--- utils/request.py
+++ utils/request.py
@@ -13,3 +13,16 @@ def getFieldsOfRequest(request, fields):
         fieldValues.append(value)
 
     return len(return_response.keys()) == 0, return_response, fieldValues
+
+
+def getUpdateFieldsOfRequest(request, fields):
+    field_values = {}
+
+    for field in fields:
+        value = request.data.get(field, None)
+        if value is None:
+            continue
+
+        field_values.update({field: value})
+
+    return len(field_values.keys()) == 0, field_values
