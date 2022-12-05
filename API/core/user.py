93fcbf4f3d4696f91f870ef31360edfcb6c38221
farhansyedain <farhansyedain@gmail.com>
2022-12-05 12:15:37 +0530

    formatted

--- API/core/user.py
+++ API/core/user.py
@@ -1,4 +1,4 @@
-from core.models import  User
+from core.models import User
 from utils.permission import IsVerfified
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
@@ -9,27 +9,27 @@ from django.contrib.auth import authenticate
 
 from utils.request import getFieldsOfRequest, getUpdateFieldsOfRequest
 
+
 @permission_classes([IsVerfified])
-@api_view(['PUT',])
+@api_view(['PUT', ])
 def update_user(request):
 
     exists, fields = getUpdateFieldsOfRequest(
         request, User().get_updatible_fields())
-    
+
     updated_fields = {}
 
-
     if not exists:
-        #Todo: Validate fields first
+        # Todo: Validate fields first
 
         User.objects.filter(email=request.user.email).update(**fields)
 
         user_object = User.objects.get(email=request.user.email)
 
         for field in fields:
-            updated_fields.update({field:getattr(user_object,field)})        
+            updated_fields.update({field: getattr(user_object, field)})
 
-    return Response(data={'code':'successfull','updated_fields':updated_fields},status=200)
+    return Response(data={'code': 'successfull', 'updated_fields': updated_fields}, status=200)
 
 
 @permission_classes([IsAuthenticated])
@@ -38,7 +38,7 @@ def delete_user(request):
 
     exists, response, fieldValues = getFieldsOfRequest(
         request, ['email', 'password'])
-    
+
     if not exists:
         return Response(data={'required': response, 'code': 'fields-not-given'}, status=400)
 
@@ -54,5 +54,5 @@ def delete_user(request):
 
     user.delete()
 
-
-    return Response(data={'code':'sucessfull'},status=201) #Todo :  status code correction
+    # Todo :  status code correction
+    return Response(data={'code': 'sucessfull'}, status=201)
