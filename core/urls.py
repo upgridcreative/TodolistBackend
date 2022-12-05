1c533e9f6c401a9cf38b6b3e723e502603fe5331
farhansyedain <farhansyedain@gmail.com>
2022-12-05 12:15:22 +0530

    Remove catagory CRUD for now

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('api/auth/login/', endpoints.login,name='api-login'),
     path('api/auth/register/', endpoints.register_user,name='api-register'),
     path('api/auth/verify/', endpoints.verify_email,name='api-verify-email'),
-
+    
     path('api/catagory/add/', endpoints.create_catagory,name='api-create-catagory'),
     path('api/catagory/delete/<pk>/', endpoints.delete_catagory,name='api-delete-catagory'),
     path('api/catagory/update/<pk>/', endpoints.update_catagory,name='api-update-catagory'),
